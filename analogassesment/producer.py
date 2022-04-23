90a1388c69c46e03c766b5cc2008174b668acb5c
Josh Cowles <cowlesjoshua@gmail.com>
2022-04-23 13:02:19 -0400

    Setup dir structure, first pass implement+test producer

---
+++ analogassesment/producer.py
@@ -0,0 +1,55 @@
+"""Producer
+
+Usage:
+    producer [--num=<count>] 
+    producer (-h | --help) 
+
+Options:
+    -h --help     Show this screen..
+    --num=<count> The number of messages to generate  [default: 1000] 
+"""
+import random
+import string
+
+from docopt import docopt
+import redis
+
+class Producer:
+    #According to what I can tell from quick search, SMS messages actually use an extension of the Latin-9 charset called 'GSM 03.38' 
+    # But given we are already limiting ourselves to only US numbers (see docs for 'generate_n_phonenumbers') 
+    # we will just use the ascii charset for now for convenience sake, although obviously this could be extended fairly easily
+    __charset = string.ascii_letters + string.digits + string.punctuation 
+    def __init__(self):
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        self.__pubsub = r.pubsub(ignore_subscribe_messages=True)
+
+    def publish(self, elmt):
+        self.__pubsub.publish('test', elmt)
+
+    def generate_n_msg(self, n):
+        count = 0
+        strLen = random.randint(1, 100)
+        while count < n:
+            yield ''.join(random.choices(self.__charset, k=strLen))
+            count += 1
+
+    # In theory, we could use the python-phonenumbers module here, but that only validates numbers, not generate them. 
+    # Similarly, we could extend this method to generate e.164 numbers (or even local numbers), 
+    # but I am considering that out of scope, as properly generating phone numbers globally is a non trivial problem of it's own.
+    def generate_n_phonenumbers(self, n):
+        count = 0
+        while count < n:
+            yield ''.join([str(random.randint(0,9)) for x in range(9)])
+            count += 1
+
+    def run(self, arguments):
+        elmt_count = arguments['--num']
+        for elmt in zip(self.generate_n_phonenumbers(elmt_count), self.generate_n_msg(elmt_count)):
+            self.publish(elmt)
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    print(arguments)
+    prd = Producer()
+    prd.run(arguments)
---
+++ tests/test_producer.py
@@ -0,0 +1,22 @@
+from unittest.mock import MagicMock
+from analogassesment.producer import Producer
+
+def test_gen_phone_number():
+    ret = list(Producer().generate_n_phonenumbers(10))
+    assert 10 == len(ret)
+    for num in ret:
+        assert 9 == len(num)
+
+def test_gen_message():
+    ret = list(Producer().generate_n_msg(10))
+    assert 10 == len(ret)
+    for msg in ret:
+        assert 1 <= len(msg) <= 100
+        
+def test_publish_called():
+    prd = Producer()
+    mock_method = MagicMock(return_value=0)
+    prd.publish = mock_method
+    prd.run({'--num': 5})
+    assert 5 == mock_method.call_count
+
